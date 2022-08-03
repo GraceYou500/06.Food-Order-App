d3e81e951c3bc2a48d641db3cb067548bb393e5c
Grace You <graceyouya500@gmail.com>
2022-08-03 16:04:49 +1000

    render cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Layout/Header';
-
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import { toBeRequired } from '@testing-library/jest-dom/dist/matchers';
 
-function App() {
+function App(props) {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <>
-      <Header />
+      {cartIsShown && <Cart onCloseCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
